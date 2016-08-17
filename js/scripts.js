89766182d9bd46d97b8f6ff6b638b24b8b2595ec
Ian Baum and Lisa MacCarrigan <ian@>
2016-08-17 14:03:34 -0400

    add logic to move consonant to end of word

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   //Business Logic
   var letter = /[a-zA-Z]/;
   var vowel = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
+  var isVowel = false;
 
   var toPigLatin = function(sentence){
     //split sentence into an array
@@ -11,6 +12,18 @@ $(document).ready(function() {
     //Add "ay" to the end of words beginning with a vowel.
     if (vowel.includes(sentenceArray[0])){
       sentenceArray.push("ay");
+    }
+    // For words beginning with one consonant, move the first consonant to the end of the word, and add "ay".
+    for (var index = 0; index < vowel.length; index++) {
+      if (sentenceArray[0] === vowel[index]){
+        isVowel = true;
+      }
+    }
+    if(!isVowel){
+      sentenceArray.push(sentenceArray[0]);
+      sentenceArray.splice(0,1);
+      sentenceArray.push("ay");
+
     }
       return sentenceArray;
   }
